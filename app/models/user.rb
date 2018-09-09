ed929933a8137abe6801e8f562f30df0cd0bc229
Aulia Rifkyano <rifkyanoa@gmail.com>
2018-09-09 20:01:23 +0000

    Update views and navigations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
-  has_many :user_stocks
-  has_many :stocks, through: :user_stocks
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+  
+  
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
          
          
   def stock_already_added?(ticker_symbol)
